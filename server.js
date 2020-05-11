28cf9d859173e14fdd72a997932077ad76de4999
HasanAhmedDev <hasanahmed.dev@gmail.com>
2020-05-12 02:45:36 +0500

    Project Structure Ready

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const connectDB = require('./config/db')
 
 const app = express()
 
+//Connecting Database
 connectDB()
 
+//Middleware
+app.use(express.json({
+    extended: false
+}))
+
 app.get('/', (req, res) => res.send('API running'))
+
+//Defining Routes
+app.use('/api/users', require('./routes/api/users'))
 
 const PORT = process.env.PORT || 5000
 
