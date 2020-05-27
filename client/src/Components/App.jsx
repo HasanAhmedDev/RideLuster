9c43592f7a9ef8c54eeadd2b09810e310d323e45
HasanAhmedDev <hasanahmed.dev@gmail.com>
2020-05-27 21:08:11 +0500

    Alerts Redux functionality added.

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -9,6 +9,7 @@ import vendor from './Vendor Interface/vendorWrapper';
 import SearchDetails from './SearchResults/SearchDetails/SearchDetails';
 import AdminPanel from './Admin/Admin';
 import BookingForm from './BookingForm/BookingForm';
+import Alert from './Utility Components/Alert';
 import { Provider } from 'react-redux';
 import store from '../store';
 
@@ -16,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
+        <Alert />
         <BrowserRouter>
           <Route exact path='/' component={Landing}></Route>
           <Route exact path='/landing' component={Landing}></Route>
--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
-import { Button, Form, Input } from "semantic-ui-react";
-import Footer from "../Footer/Footer";
+import React, { Component } from 'react';
+import { Button, Form, Input } from 'semantic-ui-react';
+import Footer from '../Footer/Footer';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 //import { Link } from 'react-router-dom';
-import "./Login.css";
+import './Login.css';
 const divStyle = {
   height: window.screen.height,
 };
 
 const initialState = {
-  email: "",
-  password: "",
-  emailErr: "",
-  passErr: "",
+  email: '',
+  password: '',
+  emailErr: '',
+  passErr: '',
 };
 
 class Login extends Component {
   state = initialState;
 
   validate = () => {
-    let emailErr = "";
-    let passErr = "";
+    let emailErr = '';
+    let passErr = '';
     if (this.state.email) {
       if (/^[A-Za-z0-9]\S*@\S+\.\S+$/.test(this.state.email) === false) {
-        emailErr = "* Email must be in a valid format.";
+        emailErr = '* Email must be in a valid format.';
       }
     } else {
-      emailErr = "* This field must be non-empty.";
+      emailErr = '* This field must be non-empty.';
     }
 
     this.setState({ emailErr });
 
     if (this.state.password) {
       if (this.state.password.length < 8) {
-        passErr = "* Password must contain atleast 8 characters.";
+        passErr = '* Password must contain atleast 8 characters.';
       }
     } else {
-      passErr = "* This field must be non-empty.";
+      passErr = '* This field must be non-empty.';
     }
 
     this.setState({ passErr });
@@ -51,6 +53,7 @@ class Login extends Component {
     let isvalid = this.validate();
     if (isvalid) {
       this.setState(initialState);
+      this.props.setAlert('Log in Successfull', 'success');
     }
   };
 
@@ -63,34 +66,34 @@ class Login extends Component {
   render() {
     //console.log(window.screen.height);
     return (
-      <div className="body" style={divStyle}>
-        <div className="overlay">
-          <div className="main-form">
-            <Form onSubmit={this.handleSubmit} className="inside-form">
-              <h4 style={{ paddingBottom: "6%" }}>Welcome to Log in</h4>
-              <Form.Field style={{ paddingBottom: "6%" }}>
+      <div className='body' style={divStyle}>
+        <div className='overlay'>
+          <div className='main-form'>
+            <Form onSubmit={this.handleSubmit} className='inside-form'>
+              <h4 style={{ paddingBottom: '6%' }}>Welcome to Log in</h4>
+              <Form.Field style={{ paddingBottom: '6%' }}>
                 <label>Email</label>
                 <Input
-                  name="email"
+                  name='email'
                   value={this.state.email}
                   onChange={this.handleChange}
-                  placeholder="Enter Email"
+                  placeholder='Enter Email'
                 />
-                <div className="valerr">{this.state.emailErr}</div>
+                <div className='valerr'>{this.state.emailErr}</div>
               </Form.Field>
-              <Form.Field style={{ paddingBottom: "6%" }}>
+              <Form.Field style={{ paddingBottom: '6%' }}>
                 <label>Password</label>
                 <Input
-                  name="password"
+                  name='password'
                   value={this.state.password}
                   onChange={this.handleChange}
-                  type="password"
-                  placeholder="Enter Password"
+                  type='password'
+                  placeholder='Enter Password'
                 />
-                <div className="valerr">{this.state.passErr}</div>
+                <div className='valerr'>{this.state.passErr}</div>
               </Form.Field>
               <Form.Field>
-                <Button fluid color="blue" type="submit">
+                <Button fluid color='blue' type='submit'>
                   Login
                 </Button>
               </Form.Field>
@@ -102,4 +105,4 @@ class Login extends Component {
     );
   }
 }
-export default Login;
+export default connect(null, { setAlert })(Login);
