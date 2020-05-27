fac5dbdaf7a1a61be61256b1bfd1fc7b8d3be77a
Ahmad <ahmad.teckstack@gmail.com>
2020-05-27 22:24:12 +0500

    auth Setup

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,11 @@
+import { ActionTypes } from '../actions/types';
+
+const initialState = {
+    token: null,
+    isAuthenticated: null,
+    loading: true
+}
+
+const setUserAuthData = (state, { authData }) => {
+
+}
